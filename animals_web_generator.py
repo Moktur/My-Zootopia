b91d7a4d5e5ca95ad3c915e12c6f6186c3a26bd7
pepe <p.zajonz@posteo.de>
2025-07-02 03:44:45 +0200

    bring animal data into html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -7,7 +7,7 @@ def load_data(file_path):
 
 
 def iterate_through_animals(animals_json_load):
-  a_info_to_print = ""
+  animal_info = ""
   for animal in animals_json_load:
     a_name = animal.get("name")
     a_diet = animal.get("characteristics", {}).get("diet")
@@ -15,17 +15,31 @@ def iterate_through_animals(animals_json_load):
     a_location = a_locations[0] if a_locations else None
     a_type = animal.get("characteristics", {}).get("type")
     if a_name and a_diet and a_location and a_type:
-      print("Name: " + a_name)
-      print("Diet: " + a_diet)
-      print("Location: " + a_location)
-      print("Type: " + a_type + "\n")
-
+      animal_info += (
+          f"Name: {a_name}\n"
+          f"Diet: {a_diet}\n"
+          f"Location: {a_location}\n"
+          f"Type: {a_type}\n"
+      )
+  return animal_info
+      
 
 def main():
 
   animals_data = load_data('animals_data.json')  
   # print(animals_data)
   iterate_through_animals(animals_data)
+  htmlfile = ""
+  with open("animals_template.html", "r", encoding="utf-8") as at:
+    template = at.read()
+    htmlfile = template.replace(
+      "__REPLACE_ANIMALS_INFO__",
+      iterate_through_animals(animals_data)
+      )
+  with open("animals_template.html", "w", encoding="utf-8") as at:
+    at.write(htmlfile)
+    
+
 
 
 if __name__ == '__main__':
